7927b970231f6b22c13ea5e8166fe01037059ab3
Wota777FF <124783591+Wota777FF@users.noreply.github.com>
2024-03-03 17:40:27 +0530

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -5,7 +5,7 @@ let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./Assets/mp3/${pickRandom(["Xlicon", "Xlicon1", "Xlicon2", "Xlicon3", "Xlicon4"])}.mp3`
 
-m.reply( `Hello ${taguser} Am Xlicon-V2 creatd by team Xlicon Need help?  type /help `)
+m.reply( `𝙂𝙍𝙀𝙀𝙏𝙄𝙉𝙂𝙎 💠 ${taguser}, This is Vortex 99,a modified version of Xlicon Botz⚡ Recoded by ＷㅤＨㅤＩㅤＴㅤＥㅤ４ㅤ４ㅤ４  ? ? Need help? type /help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
