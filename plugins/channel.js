f776a4e9f65df39e210b9c6f354431e96b7584d3
Wota777FF <124783591+Wota777FF@users.noreply.github.com>
2024-03-03 17:47:08 +0530

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,11 +1,11 @@
 let handler = async m => m.reply(`
 
-╭⭑⭑⭑★✪ *XLICON* ✪★⭑⭑⭑
-│ 📂 *BOT Name:* _XLICON-V2-MD_
-│ 📝 *Description:* _I'm XLICON-V2. A MultiDevice WhatsApp bot with rich features Created By SALMAN AMAD and Abraham Dwamena._
-│ 👤 *Owner:* _Salman Ahmad_
-│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e
-╰━━━━━━━━━━━━━━━━╯
+╭⭑⭑⭑★✪ *『 ⛓️ Vortex 444 』* ✪★⭑⭑⭑💠
+│ ⚙️ *Alias:* _Vortex_99 MultiDevice_
+│ 💠 *Description:* _This is Vortex 99, a modified and optimised version of Xlicon bots Recoded by ＷㅤＨㅤＩㅤＴㅤＥㅤ４ㅤ４ㅤ４  ?  ?._
+│ 👤 *Owner:* *ＷㅤＨㅤＩㅤＴㅤＥㅤ４ㅤ４ㅤ４  ?  ?*
+│ ♓ *Channel:* https://whatsapp.com/channel/0029VaSFe9J7oQhb9v6nqC2G
+╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
 `.trim())
 handler.help = ['channel']
 handler.tags = ['main']
